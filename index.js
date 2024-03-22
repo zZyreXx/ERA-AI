3dd83ce38b90ba5e22eca1955438ad9e58ae23bf
Tɑιғʏὗ™ <93309693+zZyreXx@users.noreply.github.com>
2024-03-22 12:41:19 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const discord = require("discord.js");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+require('dotenv').config();
+const MODEL = "gemini-pro";
+const API_KEY = process.env.API_KEY || "";
+const BOT_TOKEN = process.env.BOT_TOKEN || "";
+const CHANNEL_ID = process.env.CHANNEL_ID || "";
+
+const ai = new GoogleGenerativeAI(API_KEY);
+const model = ai.getGenerativeModel({ model: MODEL });
+
+const client = new discord.Client({
+  intents: Object.keys(discord.GatewayIntentBits),
+});
+
+client.on("ready", () => {
+  console.log("Bot is ready!");
+});
+
+client.login(BOT_TOKEN);
+
+client.on("messageCreate", async (message) => {
+  try {
+    if (message.author.bot) return;
+    if (message.channel.id !== CHANNEL_ID) return;
+
+    const { response } = await model.generateContent(message.cleanContent);
+
+    message.reply({
+      content: response.text(),
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+    console.error(error.stack);
+  }
+});
